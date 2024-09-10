99b2b4ed5a52429de240072dca1abb10b986fbd5
skrinal <richard.kamenistak@gmail.com>
2024-09-10 16:40:49 +0200

    establishment of the project

---
+++ Voxerra/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Voxerra
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
