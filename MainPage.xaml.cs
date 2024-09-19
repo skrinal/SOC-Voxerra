943752eef18373a468c161bfaf8533dbe02f7cb1
skrinal <richard.kamenistak@gmail.com>
2024-09-19 10:08:12 +0200

    skola test

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,36 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
+        /*
+        https://www.youtube.com/watch?v=r-Rm9YJzE24
+        */
+        private readonly HubConnection _connection;
         public MainPage()
         {
             InitializeComponent();
+
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://0.0.0.0:5296/chat")    
+                .Build();
+            
+            _connection.On<string>("MessageReceived", (message) =>
+            {
+                chatMessage.Text += $"{Enviroment.NewLine}{message}";
+            });
+            
+            Task.Run(() =>
+            {
+                Dispatcher.Dispatch(async () =>
+                await _connection.StartAsync());
+            });
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await _connection.InvokeCoreAsync("SendMessage", arg: []
+                { myChatMessage.Text });
+            myChatMessage.Text = String.Empty;
         }
     }
 
